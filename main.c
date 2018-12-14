40f8116f985e083efb4b467dbe7a5dbd8ba83ac3
Mr-SteveNascimento <stevecraken@gmail.com>
2018-12-14 12:06:21 -0200

    Adicinando o começo das funções de movimentação e game over

--- main.c
+++ main.c
@@ -5,27 +5,40 @@ void barra();
 void imprimir();
 void esquerda();
 void direita();
+void valoresbolinha();
+void movimentacaodabolinha();
+void gameover();
 int x=3,y=6;
+
 char matriz[9][11]; //MATRIX DO JOGO
+int movimentacao[2] = {0, 0};
+int posicaoatual[2] = {7, 5};
+int posicaoanterior[2] = {0, 0};
 
 int main(void){
 	char tecla;
 	barra(); //Chamada da funçao de inicialização ok
-	
-	while(1){
-		tecla = getch();
-		system("cls");		
-		switch(tecla){
-			case 97: esquerda();
-				break;
-			case 100: direita();      
-				break;
-			case 113: exit(0);
 
-			default: printf("Teclas Permitidas:\n (A) Esquerda  (D) Direita\n");
-				break;
-		}		
-		imprimir();	
+	while(1){
+		if(kbhit()){ // Se eu digitar alguma coisa então a compilação entrará nessa condicional, acionando a movimentação da barra
+			tecla = getch();		
+			switch(tecla){
+				case 97: esquerda();
+					break;
+				case 100: direita();      
+					break;
+				case 113: exit(0);
+	
+				default: printf("Teclas Permitidas:\n (A) Esquerda  (D) Direita\n");
+					break;
+			}
+		}
+		system("cls");
+		imprimir();
+		movimentacaodabolinha();
+		if (matriz[30][1] == 'G') { // Se a função game over for ativada, o while será interrompido
+			break;
+		}
 	}
 
 	printf("\n\n");
@@ -59,7 +72,11 @@ void imprimir(){
 	for(i=0;i<9;i++){
 		printf("|");
 		for(j=0;j<11;j++){
-			printf("%c",matriz[i][j]);
+			if (i == posicaoatual[0] && j == posicaoatual[1]) {
+				printf("O");
+			} else {
+				printf("%c",matriz[i][j]);
+			}
 		}		
 		printf("|\n");
 	}
@@ -92,4 +109,51 @@ void direita(){//Função ques move o '*' para a direita
 		x +=1;
 		y +=1;
 	}
+}
+
+void valoresbolinha() { // Função que determina os valores que serão somados ou subtraidos nas linhas e colunas
+	if (posicaoatual[0] == 7 && posicaoatual[1] <= y && posicaoatual[1] >= x) {
+		if (posicaoatual[1] <= x+1) {
+			movimentacao[0] = -1;
+			movimentacao[1] = -1;
+		}
+		else {
+			movimentacao[0] = -1;
+			movimentacao[1] = 1;
+		}
+	}
+	else if(posicaoatual[1] == 0) {
+		movimentacao[1] = 1;
+	}
+	else if(posicaoatual[1] == 11) {
+		movimentacao[1] = 1;
+	}
+	else if(posicaoatual[0] == 0) {
+		movimentacao[0] = 1;
+	}
+	else if(posicaoatual[0] == 8) {
+		gameover();
+	}
+	else {
+		posicaoatual[0] += movimentacao[0];
+		posicaoatual[1] += movimentacao[1];
+	}
+}
+void movimentacaodabolinha() { // Funcao de atribuir na 'O' a sua proxima posição;
+	valoresbolinha();
+	posicaoanterior[0] = posicaoatual[0];
+	posicaoanterior[1] = posicaoatual[1];
+	posicaoatual[0] += movimentacao[0];
+	posicaoatual[1] += movimentacao[1];
+}
+void gameover() { // Função caso vc perda o jogo
+	matriz[3][1] = 'G';
+	matriz[3][2] = 'A';
+	matriz[3][3] = 'M';
+	matriz[3][4] = 'E';
+	
+	matriz[3][6] = 'O';
+	matriz[3][7] = 'V';
+	matriz[3][8] = 'E';
+	matriz[3][9] = 'R';
 }
